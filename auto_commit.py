3def46d74ab5d46dc368f2ec34a632162993ec63
jasonhavend <jasonhaven@163.com>
2019-09-11 22:41:35 +0800

    update

--- auto_commit.py
+++ auto_commit.py
@@ -7,20 +7,27 @@ import os
 nohup python auto_commit.py &
 '''
 
+
 def update_readme():
-    now=datetime.datetime.now()
-    ts=now.strftime('%Y-%m-%d %H:%M:%S')
-    message="update at {}".format(ts)
-    os.system('git add ./*')
+    now = datetime.datetime.now()
+    ts = now.strftime('%Y-%m-%d %H:%M:%S')
+    message = "Update at {}".format(ts)
+    os.system("sed -i '$d' README.md")
+    os.system('echo "{}" >> README.md'.format(message))
+
+    os.system('git add README.md')
     os.system('git commit -m "update"')
     os.system('git push')
-    with open('log.txt','a+',encoding='utf-8') as f:    
+    
+    with open('log.txt', 'a+', encoding='utf-8') as f:
         f.write(message)
         f.write('\n')
-        print(message)
+        f.flush()
 
 if __name__ == "__main__":
     schedule.every(6).hours.do(update_readme)
+    if os.path.exists('log.txt'):
+        os.remove('log.txt')
 
     while True:
         schedule.run_pending()
